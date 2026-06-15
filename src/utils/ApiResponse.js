7244d6cb782628fe3bb69e4270567c69f5ecc223
bhumika varshney <bhumikavarshney940@gmail.com>
2026-06-15 19:16:40 +0530

    add user and video model

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,5 +1,5 @@
 class ApiResponse {
-    constructor(statuscode, message"=success", data) {
+    constructor(statuscode, message="success", data) {
         this.statusCode = status;
         this.message = message;
         this.data = data;
